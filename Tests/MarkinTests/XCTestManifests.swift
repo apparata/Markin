be991f92aff16eb580c8864bbab287ce9e990c71
Martin Johannesson <mjohannesson@gmail.com>
2018-09-22 12:21:20 +0200

    Swift 4.2

--- Tests/MarkinTests/XCTestManifests.swift
+++ Tests/MarkinTests/XCTestManifests.swift
@@ -1,6 +1,7 @@
 import XCTest
 
 #if !os(macOS)
+public typealias XCTestCaseEntry = (testCaseClass: XCTestCase.Type, allTests: [(String, (XCTestCase) throws -> Void)])
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(MarkinTests.allTests),
