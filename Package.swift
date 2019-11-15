c26214047daec4503d7009e9860d17e563634141
Martin Johannesson <mjohannesson@gmail.com>
2019-11-15 23:55:27 +0100

    Update Package.swift to Swift 5.1, remove Xcode project files as Xcode 11 can handle the Package.swift directly.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
@@ -8,7 +8,13 @@ let package = Package(
         .library(name: "Markin", targets: ["Markin"]),
     ],
     targets: [
-        .target(name: "Markin", dependencies: []),
+        .target(name: "Markin",
+                dependencies: [],
+                swiftSettings: [
+                    .define("DEBUG", .when(configuration: .debug)),
+                    .define("RELEASE", .when(configuration: .release)),
+                    .define("SWIFT_PACKAGE")
+                ]),
         .testTarget(name: "MarkinTests", dependencies: ["Markin"]),
     ]
 )
